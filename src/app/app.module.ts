0d21889011e8d496d46f21c096a2371a003d6e33
Tindleroot <tindleroot@gmail.com>
2026-03-05 03:00:00 -0800

    Update angular to 18.2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -8,19 +8,12 @@ import { AppComponent } from './app.component';
 import { DraftsComponent } from './drafts/drafts.component';
 import { MessagesComponent } from './messages/messages.component';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    DraftsComponent,
-    MessagesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        DraftsComponent,
+        MessagesComponent
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FormsModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
